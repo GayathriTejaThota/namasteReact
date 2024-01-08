080cfe6d4f4aa7c3baa724ff789f2a1150a31e57
Gayathri Thota <gayathri.thota@skillsoft.com>
2024-01-08 16:46:50 +0530

    First Reac Code

---
+++ App.js
@@ -0,0 +1,9 @@
+
+
+const parent = React.createElement("div", {id : 'parent'} , 
+                React.createElement("div", {id : 'child'} , 
+                [ React.createElement("h1", {id : 'heading'} , "This is H1"),
+                  React.createElement("h2", {id : 'heading'} , "This is H2")]));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
